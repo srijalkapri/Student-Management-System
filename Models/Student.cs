97c0efb3097a457e194a8f7ebaa44a7bb669a5ed
Srijal Kapri <srijalkapri08@gmail.com>
2026-06-16 14:12:30 +0545

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string grade { get; set; } = string.Empty;
+        public int TeacherId { get; set; }
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD.Models
+{
+    public class Teacher
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Grade { get; set; } = string.Empty;
+
+
+        public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
